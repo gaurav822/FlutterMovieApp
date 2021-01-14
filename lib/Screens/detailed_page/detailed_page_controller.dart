dc2ed8ff40aa7409836ef6683a585afbf40688a1
Gaurav Dahal <gauravdahal53@gmail.com>
2021-01-14 17:26:08 +0545

    Update detailed_page_controller.dart

--- lib/Screens/detailed_page/detailed_page_controller.dart
+++ lib/Screens/detailed_page/detailed_page_controller.dart
@@ -1,7 +1,11 @@
-
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:MovieTorrentDownloader/api/api.dart';
 import 'package:MovieTorrentDownloader/model/movie_details.dart';
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart' as pp;
+
 
 class DetailedPageController extends GetxController {
   final int id;
@@ -15,5 +19,18 @@ class DetailedPageController extends GetxController {
     MovieDetails movieDetails = await Api.movieDetails(id: id);
     casts = movieDetails.data.movie.cast;
     update();
+  }
+
+  downloadTorrent(String title, String url)async{
+    Directory downloadDir= await pp.getExternalStorageDirectory();
+    // print(downloadDir.path);
+
+    File torrentFile = File(downloadDir.path + "/" +title+".torrent");
+
+    http.Response response=await http.get(url);
+    Uint8List content= response.bodyBytes;
+    torrentFile.writeAsBytesSync(content);
+    Get.back();
+    print("download complete");
   }
 }
